f14e98da89873caaf5027bd1dfe99291599fa2c5
premiash@gmail.com <premiash@gmail.com>
2017-04-17 17:25:20 -0400

    initial routes

---
+++ app/routing/api-routes.js
@@ -0,0 +1,15 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friends = require("../data/friends");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+	
+};
