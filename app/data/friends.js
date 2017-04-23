474cbbd8581729bb3f91f8196b25529ba3cd56ed
premiash@gmail.com <premiash@gmail.com>
2017-04-22 23:09:17 -0400

    final commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -8,8 +8,38 @@
 var friends = [
   {
 	name: "John Smith",
-	photo: "https://www.pexels.com/photo/sunglasses-sunset-summer-sand-46710/",
+	photo: "https://s-media-cache-ak0.pinimg.com/200x/7d/a3/3b/7da33bd94132c9b424f685fffaf35b2b.jpg",
 	scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+  },
+  {
+	name: "Linda",
+	photo: "http://orig07.deviantart.net/1ae7/f/2014/131/5/6/profile_by_aqua1115-d7hzzc1.jpg",
+	scores: [2, 2, 3, 4, 2, 5, 4, 5, 2, 1]
+  },
+    {
+	name: "Job Samuel",
+	photo: "http://www.messagescollection.com/wp-content/uploads/2015/04/cute-profile-for-facebook.jpg",
+	scores: [5, 4, 3, 4, 2, 6, 4, 5, 5, 5]
+  },
+   	{
+	name: "Dora",
+	photo: "http://seobuzzworld.com/wp-content/uploads/2012/12/twitter-profile.jpg",
+	scores: [1, 4, 3, 4, 1, 5, 3, 5, 5, 1]
+  },
+    {
+	name: "Macintosh",
+	photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Rooster_Profile.jpg/464px-Rooster_Profile.jpg.jpg",
+	scores: [5, 1, 4, 4, 5, 5, 1, 2, 4, 5]
+  },
+    {
+	name: "Mickey",
+	photo: "https://img.clipartfest.com/519777a2c96ef06f0b590be0388fefa4_free-female-icon-clipart-profile-picture-clipart_350-350.jpeg",
+	scores: [2, 4, 5, 4, 5, 5, 1, 5, 1, 4]
+  },
+   	{
+	name: "Donald Trump",
+	photo: "http://www.messagescollection.com/wp-content/uploads/2015/04/cute-cat-profile-for-facebook.jpg",
+	scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
   }
 ];
 
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -18,7 +18,7 @@ module.exports = function(app) {
 		res.json(friends);
   	});
 
-  	app.post("/api/friends", function(req, res) {
+	app.post("/api/friends", function(req, res) {
 
   		var bestMatch = {
   			name: "",
@@ -27,14 +27,17 @@ module.exports = function(app) {
   		}
   		var userData 	= req.body;
 		var userName 	= userData.name;
-		var userImage 	= userData.image;
+		var userImage 	= userData.photo;
 		var userScores 	= userData.scores;
 
 		var totalDifference = 0;
 
 		//loop through the friends data array of objects to get each friends scores
-		for(var i = 0; i < [friends].length-1; i++){
-			console.log(friends[i].name);
+		for(var i = 0; i < friends.length; i++){
+			/*console.log(userName);
+			console.log(userImage);
+			console.log(userScores);
+			console.log(friends[i].name);*/
 			totalDifference = 0;
 
 			//loop through that friends score and the users score and calculate the 
@@ -43,8 +46,7 @@ module.exports = function(app) {
 				// We calculate the difference between the scores and sum them into the totalDifference
 				totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
 				// If the sum of differences is less then the differences of the current "best match"
-				if (totalDifference <= bestMatch.friendDifference){
-
+				if (totalDifference <= bestMatch.matchDifference){
 					// Reset the bestMatch to be the new friend. 
 					bestMatch.name = friends[i].name;
 					bestMatch.photo = friends[i].photo;
@@ -54,7 +56,7 @@ module.exports = function(app) {
 		}
 
 		friends.push(userData);
-
+		/*console.log(bestMatch);*/
 		res.json(bestMatch);
   	});
 };
